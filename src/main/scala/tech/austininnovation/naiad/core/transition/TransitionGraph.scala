0de2a8b66929ce404ba0d5fbfebd4d1e1bfc7c9a
Kevin Safford <ktsafford@gmail.com>
2018-09-26 12:45:48 +0200

    Transitions graph. Somehow not included in previous commit

---
+++ src/main/scala/tech/austininnovation/naiad/core/transition/TransitionGraph.scala
@@ -0,0 +1,48 @@
+package tech.austininnovation.naiad.core
+package transition
+
+import cats.{ Monad }
+import graph.{ GraphAlg }
+import tech.austininnovation.naiad.core.graph.{ -->, Edge, InMemoryGraph, Node }
+
+trait TransitionGraphAlg[F[_]]
+
+case class TransitionGraph[F[_]: Monad] private (
+  underlyingGraph: GraphAlg[F],
+  aggregateGraph: GraphAlg[F]) {
+
+}
+
+object TransitionGraph {
+  def apply[F[_]: Monad](underlyingGraph: GraphAlg[F]): TransitionGraph[F] = {
+    val edgeCounter: scala.collection.mutable.Map[(Node, Node), Int] = scala.collection.mutable.Map()
+    underlyingGraph.edges.foreach {
+      e =>
+        edgeCounter.update(
+          (e.left, e.right), edgeCounter.get((e.left, e.right)).getOrElse(0) + 1)
+    }
+
+    val aggregateEdges: Set[Edge] = edgeCounter.map {
+      case ((left, right), count) => Edge.create(
+        "Aggregate Edge",
+        left,
+        -->,
+        right,
+        Map("occurances" -> count))
+    }.toSet
+
+    /*
+     This is not very flexible.
+
+     TODO: Find a way to generalize the
+     TransitionGraph over different graph implimentations so we aren't
+     hard-coding the InMemoryGraph.Builder here, but instead a generic
+     GraphAlg.Builder or something.
+     */
+    val aggregateGraph = InMemoryGraph.Builder[F]
+      .addNodes(underlyingGraph.nodes)
+      .addEdges(aggregateEdges)
+      .build()
+    TransitionGraph(underlyingGraph, aggregateGraph)
+  }
+}
---
+++ src/test/scala/tech/austininnovation/naiad/core/transition/TestTransitionGraph.scala
@@ -0,0 +1,64 @@
+package tech.austininnovation.naiad.core
+package transition
+
+import org.scalatest._
+import cats.Id
+
+import graph._
+
+class TestTransitionGraph extends FlatSpec with Matchers {
+
+  val nProp1 = Map("Name" -> "A new Node")
+  val node1 = Node.create("First Node", nProp1)
+
+  val nProp2 = Map("Name" -> "An even newer node")
+  val node2 = Node.create("Second Node", nProp2)
+
+  val eProp1 = Map("Relationship" -> "Is older than")
+  val eProp2 = Map("Relationship" -> "Is connected to")
+  val eProp3 = Map("Relationship" -> "Is connected to")
+  val edge1 = Edge.create("edge1", node1, -->, node2, eProp1)
+  val edge2 = Edge.create("edge2", node1, -->, node2, eProp1)
+  val edge3 = Edge.create("edge3", node1, :-:, node1, eProp2)
+  val edge4 = Edge.create("edge4", node2, <->, node2, eProp3)
+
+  val g = InMemoryGraph.Builder[Id]
+    .addNode(node1)
+    .addNode(node2)
+    .addEdge(edge1)
+    .addEdge(edge2)
+    .addEdge(edge3)
+    .addEdge(edge4)
+    .build()
+
+  val testGraph = TransitionGraph(g)
+
+  "A transition graph " should "have nodes in the underlyingGraph" in {
+    testGraph.underlyingGraph.nodes shouldBe Set(node1, node2)
+  }
+
+  it should " have edges in the underlyingGraph" in {
+    testGraph.underlyingGraph.edges shouldBe Set(edge1, edge2, edge3, edge4)
+  }
+  "A graph" should "have nodes in the aggregateGraph" in {
+    testGraph.aggregateGraph.nodes shouldBe Set(node1, node2)
+  }
+
+  it should "have edges in the aggregateGraph" in {
+    testGraph.aggregateGraph.edges.size shouldBe 3
+  }
+
+  it should "correctly aggregate the number of edges between nodes" in {
+    testGraph.aggregateGraph.getOutEdges(node1).size shouldBe 2
+    testGraph.aggregateGraph.getOutEdges(node2).size shouldBe 1
+
+    testGraph.aggregateGraph.getEdgesBetween(node1, node2).size shouldBe 1
+    testGraph.aggregateGraph.getEdgesBetween(node1, node2).head.edgeProperties.getOrElse("occurances", None) shouldBe 2
+  }
+
+  it should "be able to return nodes and edges by their ids" in {
+    testGraph.aggregateGraph.getNodeById(node1.id) shouldBe Some(node1)
+    testGraph.underlyingGraph.getEdgeById(edge1.id) shouldBe Some(edge1)
+    testGraph.aggregateGraph.getNodeById(java.util.UUID.randomUUID()) shouldBe None
+  }
+}
